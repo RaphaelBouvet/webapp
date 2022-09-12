844b18dcb41fe2c2b21e9df28a6fbb41cb60d052
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2022-09-12 15:21:24 +0200

    model prediction works

--- app/wym/app.py
+++ app/wym/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from model_utils import summarize
 
 app = Flask(__name__)
 port = 5050
@@ -21,9 +21,10 @@ def model():
     if request.method == 'GET':
         return render_template('model.html')
     elif request.method == 'POST':
-        text_input = request.form
-        print(text_input)
-        return render_template('model_serve.html', summary = text_input)
+        text_input = request.form['texte']
+        print(f'TEXT INPUT {text_input}')
+        text_output = summarize(text_input)
+        return render_template('model_serve.html', summary = text_output[0])
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)
--- app/wym/model_utils.py
+++ app/wym/model_utils.py
@@ -7,6 +7,24 @@ test_json = {
   ]
 }
 
+def summarize(text:str):
+    json = {}
+    json['text'] = [text]
+
+    r = requests.post(
+        url = base_url,
+        json = json
+    ).json()
+    print(f'Response {r}')
+    if r['status'] == 200:
+      print(r['summary_text'])
+      summary = r['summary_text']
+    elif r['status'] == 400:
+      summary = 'BAD REQUEST'
+
+    summary = r['summary_text']
+    return summary
+
 if __name__ == '__main__':
     print('Test de Requests et text summary')
     r = requests.post(

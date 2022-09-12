7ca1380d8c7397be57521d4d3ca001cd0a3c9e79
fdac <fernandodanielarroyo90@gmail.com>
2022-09-12 15:18:22 +0200

    barre de navigation added

--- app/wym/app.py
+++ app/wym/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 port = 5050
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html', title='HOME')
 

1f0e20a362322ca665ff140f936389b6b2749f7d
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2022-09-15 14:06:48 +0200

    added word count plot

--- monitoring/monitoring/dashboard.py
+++ monitoring/monitoring/dashboard.py
@@ -33,7 +33,7 @@ st.pyplot(fig=fig)
 
 st.header('Number of request by selected sampling time')
 dict_options = {'by day':'1D', 'by hour':'1H', 'by minute':'60S'}
-choice = st.radio('Select Time Frame', options=dict_options.keys())
+choice = st.radio('Select Time Frame', options=dict_options.keys(), horizontal=True, index=2)
 table_data_ts = table_data_ts.resample(dict_options[choice]).count()
 fig, ax = plt.subplots()
 table_data_ts['time_treated'].plot(ax=ax)
@@ -49,9 +49,32 @@ ax.set_ylabel('Inference time(sec)')
 ax.set_xlabel('Text Input Len')
 st.pyplot(fig=fig)
 
+st.header('Word frequency by request')
+choice_request = st.number_input(label='selection request id', min_value=table_data.index.min(), max_value=table_data.index.max(), step=1)
+selected_text = table_data.at[choice_request, 'input_text']
+st.write('Selected_text')
+st.write(f'{selected_text}')
+
+l_words = selected_text.split(' ')
+
+word_count = {}
+for word in l_words:
+    if word in word_count:
+        word_count[word] +=1
+    else:
+        word_count[word] = 1
+sorted_count = {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
+
+fig, ax = plt.subplots()
+ax.bar(sorted_count.keys(), sorted_count.values())
+ax.set_xlabel('Word counts')
+ax.set_xticklabels('')
+st.pyplot(fig=fig)
+
 st.header('Raw Data')
 st.dataframe(data=table_data)
 
 surprise = st.button(label='Ne pas toucher debug only')
 if surprise: 
     st.snow()
+    st.success('This is a success message!', icon="✅")

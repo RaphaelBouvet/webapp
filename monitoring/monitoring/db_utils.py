4c6212b419c7701246f80fe54cae8f39f15464dd
virginiedesharnais <virginie.boniface@gmail.com>
2022-09-15 11:21:14 +0200

    Ajout des fonctions db_utils pour le dossier monitoring

---
+++ monitoring/monitoring/db_utils.py
@@ -0,0 +1,63 @@
+from email.policy import default
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.sql import func
+
+import psycopg2
+
+Base = declarative_base()
+
+
+class Text_Summ(Base):
+    __tablename__ = 'text_summarize'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    input_text = Column(String)
+    output_text = Column(String)
+    date_request = Column(DateTime, server_default=func.now())
+    time_treated = Column(Float)
+
+def get_engine():
+    return create_engine('postgresql+psycopg2://virginie:mysecretpassword@db:5432/app_v_r_d', echo=True)
+
+def verify_database():
+    engine = get_engine()
+    if not database_exists(engine.url):
+        print('Creating Database')
+        create_database(engine.url)
+        Base.metadata.create_all(engine)
+    else:
+        print('Database already exists')
+
+def get_session():
+    engine = get_engine()
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    return Session()
+
+def fetch_db(db):
+    session = get_session()
+    db_data = session.query(db).all
+    return db_data
+
+def insert_db(db, list_params):
+    if db == "User":
+        db_obj = User(**list_params)
+    elif db == "Text_Summ":
+        db_obj = Text_Summ(**list_params)
+        
+    session = get_session()
+    session.add(db_obj)
+    session.commit()
+    session.close()
+
+
+verify_database()
+engine = get_engine()
+Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    verify_database()
+    engine = get_engine()
